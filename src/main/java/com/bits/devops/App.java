1fd38c5e0ea5eef82522fecd0efc1831b57a900c
Arpit kumar Dubey <2022wa86900@wilp.bits-pilani.ac.in>
2026-09-23 23:13:41 +0530

    new commit

--- src/main/java/com/bits/devops/App.java
+++ src/main/java/com/bits/devops/App.java
@@ -1,6 +1,16 @@
 package com.bits.devops;
+
+
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Continuous Integration using Git, Maven and Jenkins");
+
+        System.out.println("DevOps Continuous Integration Project");
+
+        System.out.println("BITS ID: 2022wa86900");
+
+        System.out.println("Initial Maven Build Successful");
+
     }
+
 }
