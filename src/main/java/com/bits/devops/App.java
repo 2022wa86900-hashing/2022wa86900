9538f6686eaca3973b2313186da46fd474e1a3e1
Arpit kumar Dubey <2022wa86900@wilp.bits-pilani.ac.in>
2026-09-23 22:50:52 +0530

    First commit

---
+++ src/main/java/com/bits/devops/App.java
@@ -0,0 +1,6 @@
+package com.bits.devops;
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Continuous Integration using Git, Maven and Jenkins");
+    }
+}
